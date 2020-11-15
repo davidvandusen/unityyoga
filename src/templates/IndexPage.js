5540cd04d9fa8082459bdc3d698ca4b731cee08c
David VanDusen <github@dvw3.com>
2020-11-14 20:25:02 -0800

    Tweak schedule layout and styles

--- src/templates/IndexPage.js
+++ src/templates/IndexPage.js
@@ -12,7 +12,7 @@ export const IndexPageTemplate = ({
   yogaBlurb,
   yogaImage,
 }) => (
-  <main className="main-content wide no-padding">
+  <main className="main-content wide">
     <ol className="services-list">
       <li className="services-item">
         <Link className="services-link" to="/schedule/">
--- src/templates/SchedulePage.js
+++ src/templates/SchedulePage.js
@@ -6,34 +6,42 @@ import { graphql } from 'gatsby';
 export const SchedulePageTemplate = ({ title }) => (
   <main className="main-content wide">
     <h1 className="main-heading">{title}</h1>
-    <iframe
-      frameBorder="0"
-      height="100"
-      src="https://app.punchpass.com/embed/8932/iframe_buttons"
-      title="Unity Yoga PunchPass buttons"
-      width="100%"
-    />
-    <iframe
-      frameBorder="0"
-      height="1400"
-      src="https://app.punchpass.com/org/8932/calendar?embed=true"
-      title="Unity Yoga PunchPass calendar"
-      width="100%"
-    />
-    <iframe
-      frameBorder="0"
-      height="860"
-      src="https://app.punchpass.com/org/8932/passes?embed=true"
-      title="Unity Yoga buy PunchPass passes"
-      width="100%"
-    />
-    <iframe
-      frameBorder="0"
-      height="860"
-      src="https://app.punchpass.com/org/8932/classes?embed=true"
-      title="Unity Yoga PunchPass class list"
-      width="100%"
-    />
+    <div className="desktop-only">
+      <iframe
+        frameBorder="0"
+        height="100"
+        src="https://app.punchpass.com/embed/8932/iframe_buttons"
+        title="Unity Yoga PunchPass buttons"
+        width="100%"
+      />
+      <iframe
+        frameBorder="0"
+        height="1400"
+        src="https://app.punchpass.com/org/8932/calendar?embed=true"
+        title="Unity Yoga PunchPass calendar"
+        width="100%"
+      />
+    </div>
+    <div className="schedule-row">
+      <div className="schedule-column">
+        <iframe
+          frameBorder="0"
+          height="860"
+          src="https://app.punchpass.com/org/8932/passes?embed=true"
+          title="Unity Yoga buy PunchPass passes"
+          width="100%"
+        />
+      </div>
+      <div className="schedule-column">
+        <iframe
+          frameBorder="0"
+          height="860"
+          src="https://app.punchpass.com/org/8932/classes?embed=true"
+          title="Unity Yoga PunchPass class list"
+          width="100%"
+        />
+      </div>
+    </div>
   </main>
 );
 const SchedulePage = ({
