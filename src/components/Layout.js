525c8d183a8c3017a298b6a84b660ec2775934dd
David VanDusen <github@dvw3.com>
2020-11-25 11:36:14 -0800

    Add captions to social links

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -73,7 +73,7 @@ export const LayoutTemplate = ({
             </a>
           </div>
           <ul className="site-footer-social-list">
-            {socialImages.map(({ href, image }, index) => (
+            {socialImages.map(({ href, image, logo, text }, index) => (
               <li
                 key={String(Math.random())}
                 className={`site-footer-social-item ${
@@ -84,6 +84,25 @@ export const LayoutTemplate = ({
                   <div className="site-footer-social-image">
                     <PreviewCompatibleImage alt="social image thumbnail" src={image} />
                   </div>
+                  {!!(logo || text) && (
+                    <div className="site-footer-social-caption">
+                      {!!logo && (
+                        <div className="site-footer-social-logo">
+                          {logo === 'Instagram' && (
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 61.19 59.58">
+                              <path d="M50.09,0h-39A11,11,0,0,0,0,10.81v38A11,11,0,0,0,11.1,59.58h39a11,11,0,0,0,11.1-10.81v-38A11,11,0,0,0,50.09,0Zm-7.2,8.37A2.39,2.39,0,0,1,45.4,6h7.29a2.35,2.35,0,0,1,2.5,2.35v7a2.35,2.35,0,0,1-2,2.32,2.42,2.42,0,0,1-.47,0H45.38a2.45,2.45,0,0,1-2.47-2,1.83,1.83,0,0,1,0-.33Zm-12.3,9.31c6.8,0,12.3,5.23,12.3,11.68S37.39,41,30.59,41,18.31,35.82,18.3,29.36,23.7,17.69,30.59,17.68ZM55.19,25V50.37a2.35,2.35,0,0,1-2,2.29,4.12,4.12,0,0,1-.7.05H8.67a3.45,3.45,0,0,1-1-.11,2.35,2.35,0,0,1-1.7-2c0-.12,0-.25,0-.37V24.7h5.55a17.6,17.6,0,0,0,1.9,13.82,18.84,18.84,0,0,0,7.68,7.22A19.78,19.78,0,0,0,31.55,48a19.71,19.71,0,0,0,14.88-7.56A17.86,17.86,0,0,0,49.65,24.7h5.54Z" />
+                            </svg>
+                          )}{' '}
+                          {logo === 'Facebook' && (
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 61.19 59.58">
+                              <path d="M6.44,12.55v8.92H0V32.39H6.44V62.33H19.66V32.39h8.88s.83-5.24,1.23-11H19.71V14a3.15,3.15,0,0,1,2.87-2.62h7.21V0H20C6.11,0,6.44,10.92,6.44,12.55Z" />
+                            </svg>
+                          )}
+                        </div>
+                      )}{' '}
+                      {text}
+                    </div>
+                  )}
                 </a>
               </li>
             ))}
@@ -249,6 +268,8 @@ const layoutQuery = graphql`
               }
             }
           }
+          logo
+          text
         }
         quote
         quoteAttribution
--- src/data/layout.md
+++ src/data/layout.md
@@ -23,15 +23,27 @@ socialHeadingHref: https://www.instagram.com/explore/tags/inharmonywithnature/
 socialImages:
   - image: /img/phone-a-friend.jpg
     href: https://www.instagram.com/unityyogavancouver/
+    text: unity yoga vancouver
+    logo: Instagram
   - image: /img/susan-horning-unity-herbals.jpg
     href: https://www.instagram.com/unityherbals/
+    text: unity herbals
+    logo: Instagram
   - image: /img/e9b84921-e17f-4005-b4ba-9e2373b31258.jpeg
     href: https://www.instagram.com/unitysue/
+    text: unity sue
+    logo: Instagram
   - image: /img/d8de9903-0055-4106-b575-f0a4b68565a8_1_201_a.jpeg
     href: https://www.facebook.com/unitypartnermassage
+    text: unity partner massage
+    logo: Facebook
   - image: /img/aaa9effe-0f61-46ce-bcf4-8f8d64da4b78.jpeg
-    href: https://www.instagram.com/unityrestreats/
+    href: https://www.instagram.com/unityretreats/
+    text: unity retreats
+    logo: Instagram
   - image: /img/a73ba4aa-d341-4f19-b9b7-8f31eab77c13_1_201_a.jpeg
     href: https://www.instagram.com/unitysue/
+    text: unity sue
+    logo: Instagram
 copyright: © 2020 Unity Yoga
 ---
